f07a70bde61f7f3ab8e8d43ef0975a4d7b2e9ebb
t.abe@aegis.dip.jp <t.abe.aegis@gmail.com>
2016-09-18 21:07:51 +0900

    PIDの設定を黒の時に行うように修正

--- include/devices/sd/sd-linetrace-pid.hpp
+++ include/devices/sd/sd-linetrace-pid.hpp
@@ -24,13 +24,16 @@ class sd_linetrace_pid
 	sd_linetrace_pid(float Kp, float Ki, float Kd)
 	 : _pid(Kp, Ki, Kd), _lowpass(1.0f) {}
 	virtual void set_trace_pid(float Kp, float Ki, float Kd) {
-		_pid.set_pid(Kp, Ki, Kd);
+		_Kp = Kp;
+		_Ki = Ki;
+		_Kd = Kd;
 	}
 	virtual void set_lowpass(float q) {
-		_lowpass.set(q);
+		_lowpass_update_flag = 1;
+		_q = q;
 	}
 	virtual void clear_pid(void) {
-		_pid.clear();
+		_clear_flag = 1;
 	}
 	virtual void get_trace_pid(float &Kp, float &Ki, float &Kd) {
 		Kp = _pid.get_Kp();
@@ -42,6 +45,23 @@ class sd_linetrace_pid
 	}
 
  protected:
+	virtual void on_black_event(void) {
+		sd_linetrace::on_black_event();
+		if (_lowpass_update_flag) {
+			_lowpass.set(_q);
+			_lowpass_update_flag = 0;
+		}
+		if (_pid_update_flag) {
+			_pid.set_pid(_Kp, _Ki, _Kd);
+			_pid_update_flag = 0;
+		}
+		if (_clear_flag) {
+			_pid.clear();
+			_clear_flag = 0;
+		}
+	}
+
+ protected:
 	virtual void traveling_correction(int turn,
 					  int32_t &dps, int32_t &steering);
 
@@ -49,6 +69,10 @@ class sd_linetrace_pid
 	sd_linetrace_pid() : _pid(0.0f, 0.0f, 0.0f), _lowpass(1.0f) {};
 	low_pass_filter	_lowpass;
 	pid		_pid;
+	float 		_Kp, _Ki, _Kd, _q;
+	int32_t		_clear_flag:1;
+	int32_t		_lowpass_update_flag:1;
+	int32_t		_pid_update_flag:1;
 };
 
 NAMESPACE_SHARAKU_END
