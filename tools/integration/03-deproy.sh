e0a2b6d437b7ba9458b45d2a666fc2bfa935acda
t.abe@aegis.dip.jp <t.abe.aegis@gmail.com>
2017-01-02 15:47:30 +0900

    libsharaku-ev3devのデプロイ誤りを修正

--- tools/integration/03-deproy.sh
+++ tools/integration/03-deproy.sh
@@ -4,7 +4,6 @@ readonly DEPLOYBASE_PATH=deproy/
 readonly LIBSHARAKU_PATH=${DEPLOYBASE_PATH}libsharaku/
 readonly LIBSHARAKU_INCLUDE_PATH=${LIBSHARAKU_PATH}usr/local/include/libsharaku/
 readonly LIBSHARAKU_LOCAL_LIB_PATH=${LIBSHARAKU_PATH}usr/local/lib/libsharaku/
-readonly LIBSHARAKU_LIB_PATH=${LIBSHARAKU_PATH}usr/lib/
 readonly LIBSHARAKU_ITRONARM_INCLUDE_PATH=${LIBSHARAKU_INCLUDE_PATH}arm-itron/
 readonly LIBSHARAKU_ITRONARM_LIB_PATH=${LIBSHARAKU_LOCAL_LIB_PATH}arm-itron/
 readonly LIBSHARAKU_LINUXARM_INCLUDE_PATH=${LIBSHARAKU_INCLUDE_PATH}arm-linux/
@@ -16,7 +15,6 @@ mkdir -p ${LIBSHARAKU_ITRONARM_INCLUDE_PATH}
 mkdir -p ${LIBSHARAKU_ITRONARM_LIB_PATH}
 mkdir -p ${LIBSHARAKU_LINUXARM_INCLUDE_PATH}
 mkdir -p ${LIBSHARAKU_LINUXARM_LIB_PATH}
-mkdir -p ${LIBSHARAKU_LIB_PATH}
 
 # ---------------------------------------------------------------------------
 # libsharaku-ev3devパッケージのdeployを行う
@@ -25,12 +23,12 @@ mkdir -p ${LIBSHARAKU_LIB_PATH}
 readonly LIBSHARAKU_EV3DEV_PATH=${DEPLOYBASE_PATH}libsharaku-ev3dev/
 readonly LIBSHARAKU_EV3DEV_LIB_PATH=${LIBSHARAKU_EV3DEV_PATH}usr/local/lib/libsharaku/
 mkdir -p ${LIBSHARAKU_EV3DEV_LIB_PATH}
-mkdir -p ${LIBSHARAKU_EV3DEV_LIB_PATH}
+mkdir -p ${LIBSHARAKU_EV3DEV_PATH}usr/lib/
 cp -p build/tools/ev3dev-et.py/libEV3Way.so ${LIBSHARAKU_EV3DEV_LIB_PATH}
 cp -p build/tools/ev3dev-et.py/libHackEV.so ${LIBSHARAKU_EV3DEV_LIB_PATH}
 
-ln -s usr/local/lib/libsharaku/libEV3Way.so ${LIBSHARAKU_LIB_PATH}libEV3Way.so
-ln -s usr/local/lib/libsharaku/libHackEV.so ${LIBSHARAKU_LIB_PATH}libHackEV.so
+ln -s /usr/local/lib/libsharaku/libEV3Way.so ${LIBSHARAKU_EV3DEV_PATH}usr/lib/libEV3Way.so
+ln -s /usr/local/lib/libsharaku/libHackEV.so ${LIBSHARAKU_EV3DEV_PATH}usr/lib/libHackEV.so
 
 # ---------------------------------------------------------------------------
 # libsharakuパッケージのdeploy
