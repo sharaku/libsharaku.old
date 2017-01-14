c62a0e6373cd3a3ab46fb6cbc40251c2ac94b3de
t.abe@aegis.dip.jp <t.abe.aegis@gmail.com>
2017-01-14 11:40:54 +0900

    必要に応じて個別ビルドできるように変更

--- tools/integration/02-build.sh
+++ tools/integration/02-build.sh
@@ -5,19 +5,46 @@ mkdir build
 cd build
 
 # x86-linuxビルドを行う
-rm -f CMakeCache.txt cmake_install.cmake rm Makefile
-rm -rf CMakeFiles
-cmake ../../../ -DCMAKE_TOOLCHAIN_FILE=tools/cmake/linux-x86.cmake
-make > ../result/make.result.linux-x86 2>&1
+do_x86_linux_build()
+{
+	rm -f CMakeCache.txt cmake_install.cmake rm Makefile
+	rm -rf CMakeFiles
+	cmake ../../../ -DCMAKE_TOOLCHAIN_FILE=tools/cmake/linux-x86.cmake
+	make > ../result/make.result.linux-x86 2>&1
+}
+
 
 # arm-linuxビルドを行う
-rm -f CMakeCache.txt cmake_install.cmake rm Makefile
-rm -rf CMakeFiles
-cmake ../../../ -DCMAKE_TOOLCHAIN_FILE=tools/cmake/ev3dev-arm.cmake
-make > ../result/make.result.ev3dev-arm 2>&1
+do_arm_linux_build()
+{
+	rm -f CMakeCache.txt cmake_install.cmake rm Makefile
+	rm -rf CMakeFiles
+	cmake ../../../ -DCMAKE_TOOLCHAIN_FILE=tools/cmake/ev3dev-arm.cmake
+	make > ../result/make.result.ev3dev-arm 2>&1
+}
 
 # arm-itronビルドを行う
-rm -f CMakeCache.txt cmake_install.cmake rm Makefile
-rm -rf CMakeFiles
-cmake ../../../ -DCMAKE_TOOLCHAIN_FILE=tools/cmake/ev3rt-arm.cmake
-make > ../result/make.result.ev3rt-arm 2>&1
+do_arm_itron_build()
+{
+	rm -f CMakeCache.txt cmake_install.cmake rm Makefile
+	rm -rf CMakeFiles
+	cmake ../../../ -DCMAKE_TOOLCHAIN_FILE=tools/cmake/ev3rt-arm.cmake
+	make > ../result/make.result.ev3rt-arm 2>&1
+}
+
+case $1 in
+	x86-linux )
+		do_x86_linux_build
+		;;
+	arm-linux )
+		do_arm_linux_build
+		;;
+	arm-itron )
+		do_arm_itron_build
+		;;
+	* )
+		do_x86_linux_build
+		do_arm_linux_build
+		do_arm_itron_build
+		;;
+esac
